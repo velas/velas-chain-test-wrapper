5ce4567888764a67ceab2c9c72441df1cbdcade9
olexandr13 <oleksandr.pelykh@gmail.com>
2021-06-15 18:24:11 +0300

    fix tsconfig; att getTransactionInfo

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,40 @@
 import {
   Connection,
   PublicKey
-} from '@solana/web3.js';
-import fs from 'mz/fs';
-import os from 'os';
-import path from 'path';
-import yaml from 'yaml';
+} from '@velas/solana-web3';
+// import base58 from 'bs58';
 import { log } from '../logger';
 
 let connection: Connection;
-const publicKey = new PublicKey('EcC91Vj9AB8PqryPjHmS6w55M6fHrRA7sRzzwbYgiCoX');
-
-async function getConfig(): Promise<any> {
-  // Path to velas CLI config file
-  const CONFIG_FILE_PATH = path.resolve(
-    os.homedir(),
-    '.config',
-    'velas',
-    'cli',
-    'config.yml',
-  );
-  const configYml = await fs.readFile(CONFIG_FILE_PATH, { encoding: 'utf8' });
-  return yaml.parse(configYml);
-}
-
-async function getRpcUrl(): Promise<string> {
-  try {
-    const config = await getConfig();
-    if (!config.json_rpc_url) throw new Error('Missing RPC URL');
-    return config.json_rpc_url;
-  } catch (err) {
-    console.warn(
-      'Failed to read RPC url from CLI config file, falling back to localhost',
-    );
-    return 'http://localhost:8899';
-  }
-}
 
 async function establishConnection(): Promise<void> {
-  const rpcUrl = await getRpcUrl();
+  const rpcUrl = 'https://api.testnet.velas.com';
   connection = new Connection(rpcUrl, 'confirmed');
   const version = await connection.getVersion();
   console.log('Connection to cluster established:', rpcUrl, version);
 }
 
-async function main() {
-  await establishConnection();
+async function getBalance() {
+  const publicKey = new PublicKey('EcC91Vj9AB8PqryPjHmS6w55M6fHrRA7sRzzwbYgiCoX');
+
+  if (!connection) await establishConnection();
 
   const lamports = await connection.getBalance(publicKey);
   log.info(`Balance: ${lamports / 10 ** 9} VLX`);
 }
 
+async function getTransaction() {
+  await establishConnection();
+  const transaction = await connection.getConfirmedTransaction('6pYCFnhaMd8eZAQWT5aM1GaY75yUq6bVE7houhU9jnTKufBVb3uomzkEY2t7jRFSACn8D94rG3XgP2pos9FZXo7');
+  // log.info(`Transaction:\n${JSON.stringify(transaction, null, 2)}`);
+  // const buffer = transaction?.transaction.signatures[0].signature?.buffer as ArrayBufferLike;
+  // log.warn(Buffer.from(buffer).toString());
+  log.info(transaction?.meta?.err === null);
+  log.info(transaction?.meta?.logMessages?.join('').includes('Succeed'));
+}
+
 (async () => {
-  await main()
+  // await getBalance();
+  await getTransaction();
+
 })();
