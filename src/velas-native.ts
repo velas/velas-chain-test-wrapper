c31d3f5de07d036bf403362ac5eab076828251c9
olexandr13 <oleksandr.pelykh@gmail.com>
2021-08-09 17:44:50 +0300

    upd

--- src/velas-native.ts
+++ src/velas-native.ts
@@ -275,17 +275,17 @@ export const velasNative = new VelasNative();
   // await velasNative.getBalance('6hUNaeEwbpwEyQVgfTmZvMK1khqs18kq6sywDmRQgGyb');
 
   // 2DKco1JBu1zshWDLmCp34AVgE6YkAu9BPmgbbgRuCoGm
-  const transactionID = await velasNative.transfer({
-    payerSeed: 'delay swift sick mixture vibrant element review arm snap true broccoli industry expect thought panel curve inhale rally dish close trade damp skin below',
-    toAddress: 'DAWxo9UT6jCfCWZSoJGaU14Fjjr5boCKyNe8J6SWmcTC',
-    lamports: 501
-  }
-    , {
-      keys: [],
-      programID: 'GW5kcMNyviBQkU8hxPBSYY2BfAhXbkAraEZsMRLE36ak',
-      data: '92d8a38b-ef67-4abf-8458-8bda99eeacf13',
-    }
-  );
+  // const transactionID = await velasNative.transfer({
+  //   payerSeed: 'delay swift sick mixture vibrant element review arm snap true broccoli industry expect thought panel curve inhale rally dish close trade damp skin below',
+  //   toAddress: 'DAWxo9UT6jCfCWZSoJGaU14Fjjr5boCKyNe8J6SWmcTC',
+  //   lamports: 501
+  // }
+  //   , {
+  //     keys: [],
+  //     programID: 'GW5kcMNyviBQkU8hxPBSYY2BfAhXbkAraEZsMRLE36ak',
+  //     data: '92d8a38b-ef67-4abf-8458-8bda99eeacf13',
+  //   }
+  // );
   // log.warn(transactionID);
   // await velasNative.getBalance('6hUNaeEwbpwEyQVgfTmZvMK1khqs18kq6sywDmRQgGyb');
   // console.log(await velasNative.waitForConfirmedTransaction(transactionID));
