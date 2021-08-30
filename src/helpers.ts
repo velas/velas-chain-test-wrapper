4a4e799127fafeb4620bdce5472b9a26dd042c8b
olexandr13 <oleksandr.pelykh@gmail.com>
2021-08-30 17:44:54 +0300

    add bs58EncodedPrivateKeyToBytesArray and bytesArrayToHexString helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,5 @@
+import bs58 from "bs58";
+
 export const helpers = {
   async sleep(seconds: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
@@ -5,5 +7,16 @@ export const helpers = {
 
   stringify(json: any): string {
     return JSON.stringify(json, null, 2);
+  },
+
+  bs58EncodedPrivateKeyToBytesArray(privateKey: string): string {
+    const privateKeyAsHexString = bs58.decode(privateKey).toString('hex');
+    const privateKeyAsBytesArray = Buffer.from(privateKeyAsHexString, 'hex').toJSON().data.toString();
+
+    return privateKeyAsBytesArray;
+  },
+
+  bytesArrayToHexString(bytesArray: string): string {
+    return Buffer.from(bytesArray).toString('hex');
   },
 };
